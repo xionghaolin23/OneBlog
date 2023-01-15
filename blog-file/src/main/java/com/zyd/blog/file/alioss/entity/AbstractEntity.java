c0d3e0aab3d9dee60309a6944f9d9ce4e8dfa6ae
xionghaolin <1445695649@qq.com>
2023-01-15 19:44:41 +0800

    文件服务

---
+++ blog-file/src/main/java/com/zyd/blog/file/alioss/entity/AbstractEntity.java
@@ -0,0 +1,20 @@
+package com.zyd.blog.file.alioss.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public abstract class AbstractEntity {
+    private String bucketName;
+
+    public AbstractEntity() {
+    }
+
+    public AbstractEntity(String bucketName) {
+        this.bucketName = bucketName;
+    }
+}
